1475fece709d6d28be587b48d1a44c5b436a048a
aswinhub837 <aswinbiju306@gmail.com>
2025-11-07 15:51:54 +0530

    Add files via upload

---
+++ studentar.php
@@ -0,0 +1,15 @@
+<?php
+echo"Sorting array<br/>";
+$stud=array("nidhi","sreelekshmi","agna","riya","christy");
+echo "Displaying array using print_r()";
+print_r($stud);
+echo"<br>Array in ascending order.<br/>";
+asort($stud);
+print_r($stud);
+echo "<br>array in descending order";
+asort($stud);
+print_r($stud);
+echo "<br> Array in descending order.<br/>";
+arsort($stud);
+print_r($stud);
+?>   
